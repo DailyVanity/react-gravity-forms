df2fc278f359e9f658ffc80e7704c246f03bdd97
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-01-17 10:34:41 +0100

    finished 1.0.37

--- src/index.js
+++ src/index.js
@@ -170,10 +170,10 @@ const GravityForm = props => {
               }
             }
             const confirmationMessage = response.confirmation_message;
-            const { type, link } = confirmationMessage || false;
-            if (type && link && type === 'redirect') {
+            const { confirmation_type, confirmation_redirect } = response || false;
+            if (confirmation_type && confirmation_redirect && confirmation_type === 'redirect') {
               if (typeof window !== 'undefined') {
-                window.location.replace(link);
+                window.location.replace(confirmation_redirect);
                 return false;
               }
             }
